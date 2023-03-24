6af602c715e7da48805333dad831ba4c38515312
mineTekki <hallo@minetekki.de>
2023-03-25 00:25:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Minecraft Paper Plugin Template
+
+Dies ist ein Basis-Template für ein Minecraft Paper Plugin.
---
+++ src/main/java/de/minetekki/minecraftplugin/MinecraftPlugin.java
@@ -0,0 +1,17 @@
+package de.minetekki.minecraftplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
